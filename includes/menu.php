b76fa821f42bc3b2c12d694236ba1444235dfa4f
sohanacutweb <sohan.acutweb@gmail.com>
2018-03-09 19:17:40 +0530

    menu evening

--- includes/menu.php
+++ includes/menu.php
@@ -7,6 +7,7 @@ function social_menu_pages(){
     add_menu_page('Social Reviews', 'Social Reviews', 'manage_options', SOCIALRV_MAIN_MENU_SLUG, 'all_social_reviews', SOCIALRV_MENU_ICON);
 	
     add_submenu_page(SOCIALRV_MAIN_MENU_SLUG, 'Settings', 'Settings', 'manage_options', 'social-reviews-setting','social_setting_page' );
+	add_submenu_page(SOCIALRV_MAIN_MENU_SLUG, 'Manage Reviews', 'Manage Reviews', 'manage_options', 'social-reviews-manage','social_reviews_manage' );
 	
 }
 
