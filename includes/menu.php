d26f0bcefe367e6d7b51b3c42092c54c31d3af0b
sohanacutweb <sohan.acutweb@gmail.com>
2018-03-14 18:40:11 +0530

    add yelp in menu.php

--- includes/menu.php
+++ includes/menu.php
@@ -7,7 +7,8 @@ function social_menu_pages(){
     add_menu_page('Social Reviews', 'Social Reviews', 'manage_options', SOCIALRV_MAIN_MENU_SLUG, 'all_social_reviews', SOCIALRV_MENU_ICON);
 	
     add_submenu_page(SOCIALRV_MAIN_MENU_SLUG, 'Settings', 'Settings', 'manage_options', 'social-reviews-setting','social_setting_page' );
-	add_submenu_page(SOCIALRV_MAIN_MENU_SLUG, 'Manage Reviews', 'Manage Reviews', 'manage_options', 'social-reviews-manage','social_reviews_manage' );
+	add_submenu_page(SOCIALRV_MAIN_MENU_SLUG, 'Manage Google Reviews', 'Manage Google Reviews', 'manage_options', 'social-reviews-manage','social_reviews_manage' );
+	add_submenu_page(SOCIALRV_MAIN_MENU_SLUG, 'Manage Yelp Reviews', 'Manage Yelp Reviews', 'manage_options', 'yelp-reviews-manage','yelp_reviews_manage' );
 	
 }
 
